5deacad98b4bdcf6bba914e2336cf650c993c8ff
Kévin <93716347+Kevinr99089@users.noreply.github.com>
2024-09-05 21:41:30 +0200

    Spring cleaning (in summer)
    - Synchronising my fork with the j-hc and No-Name repos

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -31,7 +31,7 @@ else
 fi
 
 mm grep "$PKG_NAME" /proc/mounts | while read -r line; do
-	ui_print "- Un-mount"
+	ui_print "* Un-mount"
 	mp=${line#* } mp=${mp%% *}
 	mm umount -l "${mp%%\\*}"
 done
@@ -41,15 +41,15 @@ INS=true
 if BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null); then
 	BASEPATH=${BASEPATH##*:} BASEPATH=${BASEPATH%/*}
 	if [ "${BASEPATH:1:6}" = system ]; then
-		ui_print "- $PKG_NAME is a system app"
+		ui_print "* $PKG_NAME is a system app"
 	elif [ ! -d "${BASEPATH}/lib" ]; then
-		ui_print "- Invalid installation found. Uninstalling..."
+		ui_print "* Invalid installation found. Uninstalling..."
 		pm uninstall -k --user 0 "$PKG_NAME"
 	elif [ ! -f "$MODPATH/$PKG_NAME.apk" ]; then
-		ui_print "- Stock $PKG_NAME APK was not found"
+		ui_print "* Stock $PKG_NAME APK was not found"
 		VERSION=$(dumpsys package "$PKG_NAME" | grep -m1 versionName) VERSION="${VERSION#*=}"
 		if [ "$VERSION" = "$PKG_VER" ] || [ -z "$VERSION" ]; then
-			ui_print "- Skipping stock installation"
+			ui_print "* Skipping stock installation"
 			INS=false
 		else
 			abort "ERROR: Version mismatch
@@ -58,7 +58,7 @@ if BASEPATH=$(pm path "$PKG_NAME" 2>&1 </dev/null); then
 			"
 		fi
 	elif cmpr "$BASEPATH/base.apk" "$MODPATH/$PKG_NAME.apk"; then
-		ui_print "- $PKG_NAME is up-to-date"
+		ui_print "* $PKG_NAME is up-to-date"
 		INS=false
 	fi
 fi
@@ -67,7 +67,7 @@ install() {
 	if [ ! -f "$MODPATH/$PKG_NAME.apk" ]; then
 		abort "ERROR: Stock $PKG_NAME apk was not found"
 	fi
-	ui_print "- Updating $PKG_NAME to $PKG_VER"
+	ui_print "* Updating $PKG_NAME to $PKG_VER"
 	settings put global verifier_verify_adb_installs 0
 	SZ=$(stat -c "%s" "$MODPATH/$PKG_NAME.apk")
 	if ! SES=$(pm install-create --user 0 -i com.android.vending -r -d -S "$SZ" 2>&1); then
@@ -82,7 +82,7 @@ install() {
 	fi
 	if ! op=$(pm install-commit "$SES" 2>&1); then
 		if echo "$op" | grep -q INSTALL_FAILED_VERSION_DOWNGRADE; then
-			ui_print "- INSTALL_FAILED_VERSION_DOWNGRADE. Uninstalling..."
+			ui_print "* INSTALL_FAILED_VERSION_DOWNGRADE. Uninstalling..."
 			pm uninstall -k --user 0 "$PKG_NAME"
 			return 1
 		fi
@@ -100,7 +100,7 @@ if [ $INS = true ] && ! install; then abort; fi
 
 BASEPATHLIB=${BASEPATH}/lib/${ARCH}
 if [ -z "$(ls -A1 "$BASEPATHLIB")" ]; then
-	ui_print "- Extracting native libs"
+	ui_print "* Extracting native libs"
 	mkdir -p "$BASEPATHLIB"
 	if ! op=$(unzip -j "$MODPATH"/"$PKG_NAME".apk lib/"${ARCH_LIB}"/* -d "$BASEPATHLIB" 2>&1); then
 		ui_print "ERROR: extracting native libs failed"
@@ -108,10 +108,10 @@ if [ -z "$(ls -A1 "$BASEPATHLIB")" ]; then
 	fi
 	set_perm_recursive "${BASEPATH}/lib" 1000 1000 755 755 u:object_r:apk_data_file:s0
 fi
-ui_print "- Setting Permissions"
+ui_print "* Setting Permissions"
 set_perm "$MODPATH/base.apk" 1000 1000 644 u:object_r:apk_data_file:s0
 
-ui_print "- Mounting $PKG_NAME"
+ui_print "* Mounting $PKG_NAME"
 mkdir -p "/data/adb/rvhc"
 RVPATH=/data/adb/rvhc/${MODPATH##*/}.apk
 mv -f "$MODPATH/base.apk" "$RVPATH"
@@ -121,11 +121,12 @@ if ! op=$(mm mount -o bind "$RVPATH" "$BASEPATH/base.apk" 2>&1); then
 	ui_print "$op"
 fi
 am force-stop "$PKG_NAME"
-ui_print "- Optimizing $PKG_NAME"
+ui_print "* Optimizing $PKG_NAME"
 nohup cmd package compile --reset "$PKG_NAME" >/dev/null 2>&1 &
 
-ui_print "- Cleanup"
+ui_print "* Cleanup"
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
-ui_print "- Done"
+ui_print "* Done"
+ui_print "  by Kevinr99089 (github.com/Kevinr99089)"
 ui_print " "
